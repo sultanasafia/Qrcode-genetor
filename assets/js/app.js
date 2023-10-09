f69b183e32cc7ebf0f31e677f8c55349dca5a751
safiabano <safiabano736@gmail.com>
2023-10-09 09:03:27 +0500

    Qr-generator

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+
+// let imgBox = document.getElementById('imgBox')
+// let qrImg = document.getElementById('qrImg')
+// let qrText = document.getElementById('qrText')
+
+// function qrCodegen() {
+//     if(qrText.value.length > 0){
+//      qrImg.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example' + qrText.value;
+//      imgBox.classList.add("show-img");
+//     }else{
+//         qrText.classList.add('error');
+//         setTimeout(()=>{
+//         qrText.classList.remove('error');
+
+//         },1000)
+//     }
+    
+// }
+
+
+let qrText = document.getElementById('qrText')
+let imgBox = document.getElementById('imgBox')
+let qrImg = document.getElementById('qrImg')
+
+function qrCodegen(){
+    if(qrText.value.length > 0){
+        qrImg.src = ' https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example'+ qrText.value;
+        imgBox.classList.add('show-img')
+    }else{
+        qrText.classList.add('error')
+        setTimeout(()=>{
+            qrText.classList.remove('error')
+        },1000)
+    }
+    
+
+}
